851090457eb48e0bf7b89f7ee51045b4e7ac60ab
John Ferlito <johnf@inodes.org>
2013-07-06 21:17:55 +1000

    Use bundler

--- lib/capistrano/puppeteer/bootstrap.rb
+++ lib/capistrano/puppeteer/bootstrap.rb
@@ -125,7 +125,12 @@ module Capistrano
               end
               run "#{sudo} apt-key adv --keyserver keyserver.ubuntu.com --recv 4BD6EC30"
               run "#{sudo} apt-get -yq update"
-              run "#{sudo} apt-get install -y puppet libaugeas-ruby git"
+              run "cd /tmp && wget http://us-west-2.ec2.archive.ubuntu.com/ubuntu/pool/universe/r/ruby-thor/ruby-thor_0.15.3-1_all.deb"
+              run "cd /tmp && wget http://us-west-2.ec2.archive.ubuntu.com/ubuntu/pool/universe/r/ruby-net-http-persistent/ruby-net-http-persistent_2.7-2_all.deb"
+              run "cd /tmp && wget http://us-west-2.ec2.archive.ubuntu.com/ubuntu/pool/universe/b/bundler/bundler_1.2.3-1ubuntu1_all.deb"
+              run "cd /tmp && #{sudo} dpkg -i *deb"
+              run "#{sudo} apt-get purge ruby1.8.7"
+              run "#{sudo} apt-get install -y puppet libaugeas-ruby git ruby1.9.3"
 
               unless remote_file_exists? puppet_path
                 run "git clone #{puppet_repo} /tmp/puppet"
