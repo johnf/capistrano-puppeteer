a30a97e3f044e3cf2ea78309eb3f4469d40939d3
John Ferlito <johnf@inodes.org>
2013-12-14 19:20:24 +1100

    make sure we remove ruby 1.8

--- lib/capistrano/puppeteer/bootstrap.rb
+++ lib/capistrano/puppeteer/bootstrap.rb
@@ -126,14 +126,14 @@ module Capistrano
               end
               run "#{sudo} apt-key adv --keyserver keyserver.ubuntu.com --recv 4BD6EC30"
               run "#{sudo} apt-get -yq update"
-              run "#{sudo} apt-get purge -y ruby1.8 ruby"
+              run "#{sudo} apt-get purge -y ruby1.8 ruby libruby1.8 libruby libshadow-ruby1.8"
               run "#{sudo} apt-get install -y ruby1.9.3"
               run "cd /tmp && wget http://us-west-2.ec2.archive.ubuntu.com/ubuntu/pool/universe/r/ruby-thor/ruby-thor_0.15.3-1_all.deb"
               run "cd /tmp && wget http://us-west-2.ec2.archive.ubuntu.com/ubuntu/pool/universe/r/ruby-net-http-persistent/ruby-net-http-persistent_2.7-2_all.deb"
               run "cd /tmp && wget http://us-west-2.ec2.archive.ubuntu.com/ubuntu/pool/universe/b/bundler/bundler_1.2.3-1ubuntu1_all.deb"
               run "cd /tmp && wget http://us-west-2.ec2.archive.ubuntu.com/ubuntu/pool/main/r/ruby-shadow/ruby-shadow_2.1.4-2_amd64.deb"
               run "cd /tmp && #{sudo} dpkg -i *deb"
-              run "#{sudo} apt-get install -y puppet libaugeas-ruby git"
+              run "#{sudo} apt-get install -y puppet libaugeas-ruby1.9.1 git"
               run "#{sudo} apt-get purge -y ruby1.8 ruby"
 
               unless remote_file_exists? puppet_path
